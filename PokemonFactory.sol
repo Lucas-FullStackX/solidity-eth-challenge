7497337c7ba11c8da61373ea04a28d1aafe95889
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2022-09-18 15:11:45 -0500

    add abilities

--- PokemonFactory.sol
+++ PokemonFactory.sol
@@ -3,23 +3,38 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
+    struct Ability {
+        string name;
+        string description;
+    }
     struct Pokemon {
         uint256 id;
         string name;
+        Ability[] abilities;
     }
 
     Pokemon[] private pokemons;
-    event NewPokemon(address owner, uint256 id, string name);
+    event NewPokemon(Pokemon newPokemon);
     mapping(uint256 => address) public pokemonToOwner;
     mapping(address => uint256) ownerPokemonCount;
 
-    function createPokemon(string memory _name, uint256 _id) public {
+    function createPokemon(
+        string memory _name,
+        uint256 _id,
+        Ability[] memory _abilities
+    ) public {
         require(_id > 0, "Invalid ID");
         require(bytes(_name).length > 3, "Invalid name");
-        pokemons.push(Pokemon(_id, _name));
+        pokemons.push();
+        uint256 index = pokemons.length - 1;
+        pokemons[index].id = _id;
+        pokemons[index].name = _name;
+        for (uint256 i = 0; i < _abilities.length; i++) {
+            pokemons[index].abilities.push(_abilities[i]);
+        }
         pokemonToOwner[_id] = msg.sender;
         ownerPokemonCount[msg.sender]++;
-        emit NewPokemon(msg.sender, _id, _name);
+        emit NewPokemon(pokemons[index]);
     }
 
     function getAllPokemons() public view returns (Pokemon[] memory) {
